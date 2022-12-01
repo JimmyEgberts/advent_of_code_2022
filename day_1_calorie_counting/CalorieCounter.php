5db2c2f55269fe68b7077af7bfc437d05ebde4af
Jimmy Egberts <jimmyegberts@hotmail.com>
2022-12-01 22:01:17 +0100

    feat: Created a function to seperate and add the input into groups of calories.

--- day_1_calorie_counting/CalorieCounter.php
+++ day_1_calorie_counting/CalorieCounter.php
@@ -10,4 +10,21 @@ function Read_Input(string $File_Path): string
     return $Input;
 }
 
+function Get_Calorie_Groups($Calorie_Array): array
+{
+    $Calorie_Groups = [];
+    $Current_Calories = 0;
+    for ($i = 0; $i < sizeof($Calorie_Array); $i++) {
+        if ($Calorie_Array[$i] == null) {
+            $Calorie_Groups[] = $Current_Calories;
+            $Current_Calories = 0;
+        } else {
+            $Current_Calories += $Calorie_Array[$i];
+        }
+    }
+    return $Calorie_Groups;
+}
+
 $Input = Read_Input("input.txt");
+$Calorie_Array = explode(PHP_EOL, $Input);
+$Calorie_Groups = Get_Calorie_Groups($Calorie_Array);
