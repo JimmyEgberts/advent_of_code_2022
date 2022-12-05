bfd659112ad44eabd864aff72778ee1f7e1a7880
Jimmy Egberts <jimmyegberts@hotmail.com>
2022-12-05 04:32:25 +0100

    feat: Create a new function to count every overlapping assignment and print this out.

--- day_4_camp_cleanup/AssignmentAssigner.php
+++ day_4_camp_cleanup/AssignmentAssigner.php
@@ -29,8 +29,41 @@ function Get_Fully_Overlapping_Assignments($Assignment_Pairs_Array): string
     return $Fully_Overlapping_Assignments;
 }
 
+function Get_Partially_Overlapping_Assignments($Assignment_Pairs_Array): string
+{
+    $Overlapping_Assignments = 0;
+
+    foreach ($Assignment_Pairs_Array as $Assignment_Pairs) {
+        $Assignments = explode(",", $Assignment_Pairs);
+        $First_Assignment = explode("-", $Assignments[0]);
+        $Second_Assignment = explode("-", $Assignments[1]);
+
+        $First_Assignment_Range = range($First_Assignment[0], $First_Assignment[1]);
+        $Second_Assignment_Range = range($Second_Assignment[0], $Second_Assignment[1]);
+
+        foreach ($First_Assignment_Range as $First_Range_Number) {
+            // Loop through each number in the second set
+            foreach ($Second_Assignment_Range as $Second_Range_Number) {
+                // If any number in the first set is equal to any number in the second set,
+                // then the sets overlap
+                if ($First_Range_Number == $Second_Range_Number) {
+                    $Overlapping_Assignments += 1;
+                    break 2;
+                }
+            }
+        }
+    }
+
+    return $Overlapping_Assignments;
+}
+
 $Input = Read_Input("input.txt");
 $Assignment_Pairs_Array = explode(PHP_EOL, $Input);
 $Fully_Overlapping_Assignments = Get_Fully_Overlapping_Assignments($Assignment_Pairs_Array);
 
 printf("The amount of fully overlapping assignments is: %d." . PHP_EOL, $Fully_Overlapping_Assignments);
+
+// Part 2
+
+$Partially_Overlapping_Assignments = Get_Partially_Overlapping_Assignments($Assignment_Pairs_Array);
+printf("The amount of partially overlapping assignments is: %d." . PHP_EOL, $Partially_Overlapping_Assignments);
