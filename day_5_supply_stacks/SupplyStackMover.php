948afa30c0753d4b90ff830b73ce8f64d5f7d8b9
Jimmy Egberts <jimmyegberts@hotmail.com>
2022-12-05 17:02:45 +0100

    feat: Created parsers for the input files and executed the parsed instructions on the parsed box arrangement.

---
+++ day_5_supply_stacks/SupplyStackMover.php
@@ -0,0 +1,53 @@
+<?php
+
+// Part one
+
+function Read_Input(string $File_Path): string
+{
+    $File = fopen($File_Path, "r");
+    $Input = fread($File, filesize($File_Path));
+    fclose($File);
+    return $Input;
+}
+
+function Parse_Starting_Stacks($Starting_Stacks_Drawing_Array): array
+{
+    $Stacks = [];
+    foreach ($Starting_Stacks_Drawing_Array as $Box_Line) {
+        for ($Index = 1; $Index < count(str_split($Box_Line)); $Index += 4) {
+            if (!ctype_space($Box_Line[$Index]) && !ctype_digit($Box_Line[$Index])) {
+                $Stack_Index = ($Index - 1) / 4;
+                $Stacks[$Stack_Index][] = $Box_Line[$Index];
+            }
+        }
+    }
+
+    return $Stacks;
+}
+
+function Execute_Rearrangement_Procedure($Starting_Stacks, $Rearrangement_Procedure_Array): array
+{
+    $New_Stacks = $Starting_Stacks;
+    foreach ($Rearrangement_Procedure_Array as $Rearrangement_Procedure) {
+        $Words = explode(' ', $Rearrangement_Procedure);
+        $Number_Of_Boxes = $Words[1];
+        $First_Stack_Index = $Words[3] - 1;
+        $Second_Stack_Index = $Words[5] - 1;
+
+        for ($Index = 0; $Index < $Number_Of_Boxes; $Index++) {
+            $Box = array_shift($New_Stacks[$First_Stack_Index]);
+            array_unshift($New_Stacks[$Second_Stack_Index], $Box);
+        }
+    }
+
+    return $New_Stacks;
+}
+
+$Starting_Stacks_Drawing_Input = Read_Input("starting_stacks_drawing.txt");
+$Starting_Stacks_Drawing_Array = explode(PHP_EOL, $Starting_Stacks_Drawing_Input);
+$Starting_Stacks = Parse_Starting_Stacks($Starting_Stacks_Drawing_Array);
+
+$Rearrangement_Procedure_Input = Read_Input("rearrangement_procedure.txt");
+$Rearrangement_Procedure_Array = explode(PHP_EOL, $Rearrangement_Procedure_Input);
+$New_Stacks = Execute_Rearrangement_Procedure($Starting_Stacks, $Rearrangement_Procedure_Array);
+
