aaa68929a4e8207d9e36be726dbdbdb43d7eb229
Jimmy Egberts <jimmyegberts@hotmail.com>
2022-12-07 02:13:36 +0100

    feat: Created a function that accepts the simulated datastream and returns both the position and the four characters of the start-of-packet marker.

--- day_6_tuning_trouble/FixCommunicationSystem.php
+++ day_6_tuning_trouble/FixCommunicationSystem.php
@@ -10,16 +10,35 @@ function Read_Input(string $File_Path): string
     return $Input;
 }
 
-function Create_Simulated_Data_Buffer($DataString) {
-    foreach (str_split($DataString) as $Character) {
+function Create_Simulated_Datastream_Buffer($DataString): mixed
+{
+    foreach (str_split($DataString) as $Character)
+    {
         yield $Character;
-        usleep(10000);
+        // usleep(1000);
     }
 }
 
-$Data_Stream_Input = Read_Input("input.txt");
-$Simulated_Data_Stream_Buffer = Create_Simulated_Data_Buffer($Data_Stream_Input);
-
-foreach ($Simulated_Data_Stream_Buffer as $Character) {
-    echo $Character;
+function Get_Start_Of_Packet_Marker($Simulated_Datastream_Buffer): mixed
+{
+    $Data_Stream = "";
+    $Start_Of_Packet_Marker = "";
+    foreach ($Simulated_Datastream_Buffer as $Character)
+    {
+        $Data_Stream .= $Character;
+    
+        if (strlen($Data_Stream) > 3) {
+            $Start_Of_Packet_Marker = substr($Data_Stream, strlen($Data_Stream) - 4);
+            if (sizeof(array_unique(str_split($Start_Of_Packet_Marker))) == 4) {
+                break;
+            }
+        }
+    }
+    return [$Start_Of_Packet_Marker, strlen($Data_Stream)];
 }
+
+$Datastream_Input = Read_Input("input.txt");
+$Simulated_Datastream_Buffer = Create_Simulated_Datastream_Buffer($Datastream_Input);
+$Start_Of_Packet_Marker_Object = Get_Start_Of_Packet_Marker($Simulated_Datastream_Buffer);
+
+printf("The start-of-packet marker is located at position %d, and is the following four letters: %s.", $Start_Of_Packet_Marker_Object[1], $Start_Of_Packet_Marker_Object[0]);
