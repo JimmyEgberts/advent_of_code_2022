6d523a8f63e7fe58ddac956ae53c6adcec7de7d1
Jimmy Egberts <jimmyegberts@hotmail.com>
2022-12-07 02:33:20 +0100

    feat: The datastream is now processed and checked for both start-of-packet and start-of-message markers.

--- day_6_tuning_trouble/FixCommunicationSystem.php
+++ day_6_tuning_trouble/FixCommunicationSystem.php
@@ -19,26 +19,48 @@ function Create_Simulated_Datastream_Buffer($DataString): mixed
     }
 }
 
-function Get_Start_Of_Packet_Marker($Simulated_Datastream_Buffer): mixed
+function Get_Marker($Datastream_String, $Marker_Size): mixed
 {
-    $Data_Stream = "";
     $Start_Of_Packet_Marker = "";
-    foreach ($Simulated_Datastream_Buffer as $Character)
-    {
-        $Data_Stream .= $Character;
-    
-        if (strlen($Data_Stream) > 3) {
-            $Start_Of_Packet_Marker = substr($Data_Stream, strlen($Data_Stream) - 4);
-            if (sizeof(array_unique(str_split($Start_Of_Packet_Marker))) == 4) {
-                break;
-            }
+    if (strlen($Datastream_String) > $Marker_Size - 1) {
+        $Start_Of_Packet_Marker = substr($Datastream_String, strlen($Datastream_String) - $Marker_Size);
+        if (sizeof(array_unique(str_split($Start_Of_Packet_Marker))) == $Marker_Size) {
+            return [$Start_Of_Packet_Marker, strlen($Datastream_String)];
         }
     }
-    return [$Start_Of_Packet_Marker, strlen($Data_Stream)];
+
+    return false;
+}
+
+function Process_Datastream_Buffer($Simulated_Datastream_Buffer): array
+{
+    $Datastream_String = "";
+    $Start_Of_Packet_Marker_Object = false;
+    $Start_Of_Message_Marker_Object = false;
+
+    foreach ($Simulated_Datastream_Buffer as $Character)
+    {
+        $Datastream_String .= $Character;
+
+        if (!$Start_Of_Packet_Marker_Object) {
+            $Start_Of_Packet_Marker_Object = Get_Marker($Datastream_String, 4);
+        }
+
+        if (!$Start_Of_Message_Marker_Object) {
+            $Start_Of_Message_Marker_Object = Get_Marker($Datastream_String, 14);
+        }
+    }
+
+    return [$Start_Of_Packet_Marker_Object, $Start_Of_Message_Marker_Object];
 }
 
 $Datastream_Input = Read_Input("input.txt");
-$Simulated_Datastream_Buffer = Create_Simulated_Datastream_Buffer($Datastream_Input);
-$Start_Of_Packet_Marker_Object = Get_Start_Of_Packet_Marker($Simulated_Datastream_Buffer);
 
-printf("The start-of-packet marker is located at position %d, and is the following four letters: %s.", $Start_Of_Packet_Marker_Object[1], $Start_Of_Packet_Marker_Object[0]);
+$Simulated_Datastream_Buffer = Create_Simulated_Datastream_Buffer($Datastream_Input);
+$Markers = Process_Datastream_Buffer($Simulated_Datastream_Buffer);
+
+printf("The start-of-packet marker is located at position %d, and is the following four letters: \"%s\"." . PHP_EOL, $Markers[0][1], $Markers[0][0]);
+
+// Part 2
+
+printf("The start-of-message marker is located at position %d, and is the following four letters: \"%s\"." . PHP_EOL, $Markers[1][1], $Markers[1][0]);
